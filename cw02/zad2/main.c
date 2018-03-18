3fed38c0a66d762273e1349821c16ff50c69c3d3
Szymon <szymon@MacSzymon.local>
2018-03-18 19:44:17 +0100

    generating implemented

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
-#include <ftw.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #define _XOPEN_SOURCE 500
+
+void generate(char *path, int amount, int length){
+    char *string = malloc(length * sizeof(char) + 1);
+    FILE *f = fopen(path, "w+");
+    FILE *rand0m = fopen("/dev/random","r");
+
+    for (int i=0; i<amount; i++){
+        if (fread(string,sizeof(char),(size_t) length + 1, rand0m) != length + 1){
+            printf("Problem with reading chars from /dev/random\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int k=0;k<length;k++)
+            string[k]= (char) abs(string[k]) % 94 + 33;
+
+        string[length] = 10;
+        if (fwrite(string,sizeof(char),(size_t) length + 1,f) != length + 1){
+            printf("Problem with writing generated string to file\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    fclose(rand0m);
+    fclose(f);
+    free(string);
+}
+
+
 
 int main(int argc, char **argv){
     nftw();

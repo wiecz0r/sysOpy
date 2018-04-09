fb20c7d1d43c8a5174e1fae7ca24e7f18ee54bb8
Szymon <szymon@MacSzymon.local>
2018-04-09 22:25:05 +0200

    Complete ex1

---
+++ cw04/zad1/main.c
@@ -0,0 +1,83 @@
+#define _DEFAULT_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+
+int paused = 0;
+int killed = 0;
+static char* cmd = "./time.sh";
+
+void SIGTSTP_handler(int signal){
+    if (!paused)
+        printf("\nOczekuję na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu\n");
+    paused = (paused+1)%2;
+}
+
+void SIGINT_handler(int signal){
+    printf("\nOdebrano sygnał SIGINT\n");
+    exit(EXIT_SUCCESS);
+}
+
+void child(){
+    pid_t pid = fork();
+    if(!pid) execvp(cmd,NULL);
+
+    while(1){
+        if (!paused){
+            if(killed){
+                killed = 0;
+                pid = fork();
+                if(!pid) execvp(cmd,NULL);
+            }
+        }
+        else if (!killed){
+            kill(pid,SIGKILL);
+            killed = 1;
+        }
+    }
+}
+
+int main(int args, char **argv){
+    if (args != 2){
+        printf("Podaj argument: 1 - wersja normalna, 2 - wersja z procesami\n");
+        exit(EXIT_FAILURE);
+    }
+    struct sigaction sigact;
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_handler = SIGTSTP_handler;
+    sigact.sa_flags = 0;
+
+    sigaction(SIGTSTP,&sigact,NULL);
+    signal(SIGINT,SIGINT_handler);
+    
+    int arg = atoi(argv[1]);
+
+    // WERSJA NORMALNA
+    if (arg == 1){
+        printf("Wersja normalna\n");
+        time_t myTime;
+        struct tm *tmInfo;
+        
+        while(1){
+            if(!paused){
+                myTime = time(NULL);
+                tmInfo = localtime(&myTime);
+                printf("\nAktualny czas: %s",asctime(tmInfo));
+            }
+            sleep(1);
+        }
+        return 0;
+    }
+
+    //WERSJA Z PROCESAMI
+    else if (arg == 2){
+        printf("Wersja z procesami\n");
+        child();
+        return 0;
+    }
+    return 1;
+}
+
---
+++ cw04/zad1/time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+    printf "\n"
+    echo `date`
+    sleep 1
+done

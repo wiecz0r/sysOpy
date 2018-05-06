3abdfd637ebf78cdb89b9f842d0c42f374601fcb
Szymon <szymon@MacBook-Air.local>
2018-05-06 23:53:10 +0200

    .

--- cw06/zad1/sV.h
+++ cw06/zad1/sV.h
@@ -1,25 +1,26 @@
 #ifndef HEADER
 #define HEADER
 
-#define MIRROR  1
-#define CALC    2
-#define TIME    3
-#define END     4
-#define INIT    5
-#define STOP    6
-#define RPLY    7
-#define ERR     8
-#define UNDEF   9
+#define INIT    1
+#define MIRROR  2
+#define CALC    3
+#define TIME    4
+#define END     5
+#define TMC     6
 
-#define MAX_MSG_TXT 4096
-#define MSG_SIZE     sizeof(struct msg_buf)-sizeof(long)
+#define MAX_MSG_TXT 300
 #define MAX_CLIENTS 10
+
+#define PROJECT_ID 12
 
 typedef struct msg_buf 
 {
     long msg_type;
     char msg_text[MAX_MSG_TXT];
     int client_id;
+    int client_PID;
 } msg_buf;
+
+const size_t MSG_SIZE = 500;
 
 #endif
